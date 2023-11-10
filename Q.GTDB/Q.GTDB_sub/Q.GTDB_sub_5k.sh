7a9c541bb91f230a9bae9ee9b4bd3ef1e51c0bb6
roblanf <rob.lanfear@gmail.com>
2023-11-10 09:50:48 +0900

    the 5k matrix is done
    Let's see if it's any good now...

--- Q.GTDB/Q.GTDB_sub/Q.GTDB_sub_5k.sh
+++ Q.GTDB/Q.GTDB_sub/Q.GTDB_sub_5k.sh
@@ -1,28 +1,52 @@
 analysis="Q.GTDB_sub"
 training="training_loci_5k"
+name="5k"
 
 # 5. Estimate the models
+log=$training"_log.txt"
 
-echo "Estimating initial models with IQ-TREE2 for $training" >> log.txt
+echo "Log file for "$training > $log
+echo "Estimating initial models with IQ-TREE2 for" $training >> $log
 
 mkdir 02_fullcon_$training # first make the output directory
 model_set="LG,Q.pfam,Q.insect,Q.yeast"
-iqtree2 -T 200 -st AA -S $training -cmax 4 -mset $model_set -pre 02_fullcon_$training/iteration_1
+iqtree2 -T 128 -st AA -S $training -cmax 4 -mset $model_set -pre 02_fullcon_$training/iteration_1
 
 # get the list of models, and save it to models.txt
 grep '^ *[^ ]\+:' 02_fullcon_$training/iteration_1.best_scheme.nex | awk -F: '{print $1}' | awk '{print $NF}' | cut -d'+' -f1 | sort | uniq -c | sort -nr > 02_fullcon_$training/models.txt
 
-echo "List of models best fit to training loci: " >> log.txt
-cat 02_fullcon_$training/models.txt >> log.txt
+echo "List of models best fit to training loci: " >> $log
+cat 02_fullcon_$training/models.txt >> $log
 
 # now we get the init model as the first model in that list
 
 initial_model=$(awk 'NR==1 {print $2}' 02_fullcon_$training/models.txt)
-echo "Initial Model will be set to" >> log.txt
-echo $initial_model >> log.txt
+echo "Initial Model will be set to" >> $log
+echo $initial_model >> $log
 
 # 7. Estimate the Q matrix
 
-echo "Estimating Q matrix with IQ-TREE2" >> log.txt
+echo "Estimating Q matrix with IQ-TREE2" >> $log
+echo "Using LG as the initial model due to IQ-TREE bug!" >> $log
 
-iqtree2 -T 200 -S 02_fullcon_$training/iteration_1.best_scheme.nex -te 02_fullcon_$training/iteration_1.treefile --init-model LG --model-joint GTR20+FO -pre 02_fullcon_$training/iteration_1.GTR20
+iqtree2 -T 128 -S 02_fullcon_$training/iteration_1.best_scheme.nex -te 02_fullcon_$training/iteration_1.treefile --init-model LG --model-joint GTR20+FO -pre 02_fullcon_$training/iteration_1.GTR20
+
+
+# extract the matrix
+new_model="Q.GTDB_sub_"$name
+echo "" >> $log
+echo "## Model Testing ##" >> $log
+echo "Extracting model and saving to "$new_model >> $log
+
+grep -A 21 "can be used as input for IQ-TREE" 02_fullcon_$training/iteration_1.GTR20.iqtree | tail -n20 > $new_model
+
+cat $new_model >> $log
+
+
+# # test it on test loci
+echo "" >> $log
+echo "Testing model on test loci..." >> $log
+mkdir 03_testing_$training
+iqtree2 -T 248 -S testing_loci/ -st AA -m MF -mset LG,Q.pfam,Q.insect,Q.yeast,$new_model -pre 03_testing_$training/test_loci_mf
+echo "Frequency table of best-fit models" >> $log
+grep '^ *[^ ]\+:' 03_testing_$training/test_loci_mf.best_scheme.nex | awk -F: '{print $1}' | awk '{print $NF}' | cut -d'+' -f1 | sort | uniq -c | sort -nr >> $log
--- Q.GTDB/README.md
+++ Q.GTDB/README.md
@@ -1252,35 +1252,6 @@ echo "training_loci_50k: $n50k" >> log.txt
 
 ```
 
-Now we estimate the models, but we need to do this once for every training dataset...
+Now we estimate the models, but we need to do this once for every training dataset.
 
-```{bash}
-analysis="Q.GTDB_sub"
-training="training_loci_1k"
-
-# 5. Estimate the models
-
-echo "Estimating initial models with IQ-TREE2 for $training" >> log.txt
-
-mkdir 02_fullcon_$training # first make the output directory
-model_set="LG,Q.pfam,Q.insect,Q.yeast"
-iqtree2 -T 100 -st AA -S $training -cmax 4 -mset $model_set -pre 02_fullcon_$training/iteration_1
-
-# get the list of models, and save it to models.txt
-grep '^ *[^ ]\+:' 02_fullcon_$training/iteration_1.best_scheme.nex | awk -F: '{print $1}' | awk '{print $NF}' | cut -d'+' -f1 | sort | uniq -c | sort -nr > 02_fullcon_$training/models.txt
-
-echo "List of models best fit to training loci: " >> log.txt
-cat 02_fullcon_$training/models.txt >> log.txt
-
-# now we get the init model as the first model in that list
-
-initial_model=$(awk 'NR==1 {print $2}' 02_fullcon_$training/models.txt)
-echo "Initial Model will be set to" >> log.txt
-echo $initial_model >> log.txt
-
-# 7. Estimate the Q matrix
-
-echo "Estimating Q matrix with IQ-TREE2" >> log.txt
-
-iqtree2 -T 100 -S 02_fullcon_$training/iteration_1.best_scheme.nex -te 02_fullcon_$training/iteration_1.treefile --init-model LG --model-joint GTR20+FO -pre 02_fullcon_$training/iteration_1.GTR20
-```
+This is initially stored in `Q.GTDB_sub_5k.sh`
