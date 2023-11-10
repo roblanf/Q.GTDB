9baa4e0f024d7a1a6dd16f63bfe4633ad1f9fe41
roblanf <rob.lanfear@gmail.com>
2023-11-10 14:24:54 +0900

    Finalise script for 5k analysis
    should be easy to run the 100 and 1K analyses now.

--- Q.GTDB/Q.GTDB_sub/Q.GTDB_sub_5k.sh
+++ Q.GTDB/Q.GTDB_sub/Q.GTDB_sub_5k.sh
@@ -1,6 +1,8 @@
 analysis="Q.GTDB_sub"
 training="training_loci_5k"
 name="5k"
+new_model="Q.GTDB_sub_"$name
+
 
 # 5. Estimate the models
 log=$training"_log.txt"
@@ -33,7 +35,6 @@ iqtree2 -T 128 -S 02_fullcon_$training/iteration_1.best_scheme.nex -te 02_fullco
 
 
 # extract the matrix
-new_model="Q.GTDB_sub_"$name
 echo "" >> $log
 echo "## Model Testing ##" >> $log
 echo "Extracting model and saving to "$new_model >> $log
@@ -50,3 +51,56 @@ mkdir 03_testing_$training
 iqtree2 -T 248 -S testing_loci/ -st AA -m MF -mset LG,Q.pfam,Q.insect,Q.yeast,$new_model -pre 03_testing_$training/test_loci_mf
 echo "Frequency table of best-fit models" >> $log
 grep '^ *[^ ]\+:' 03_testing_$training/test_loci_mf.best_scheme.nex | awk -F: '{print $1}' | awk '{print $NF}' | cut -d'+' -f1 | sort | uniq -c | sort -nr >> $log
+
+
+# test it on the full and the reduced datasets
+# we don't need to do LG since we did that for phylum_1...
+
+# small alignment
+raxml-ng --msa ../concat_alignments/gtdb_r207_bac120_concatenated.faa --model PROTGTR{$new_model}+G --threads 16 --force perf_threads --tree r207_original_clean.tree --evaluate --lh-epsilon 0.1  --prefix 03_testing_$training/$new_model"_G_reduced_aln"
+
+# big alignment
+raxml-ng --msa ../concat_alignments/gtdb_r207_bac120_full.faa --model PROTGTR{$new_model}+G --threads 16 --force perf_threads --tree r207_original_clean.tree --evaluate --lh-epsilon 0.1  --prefix 03_testing_$training/$new_model"_G_full_aln"
+
+
+# finally we make a little table of the likelihoods etc. for the analyses, and compare it to LG
+
+# Specify your list of filenames
+f1=03_testing_$training/$new_model"_G_reduced_aln.raxml.log"
+f2="../phylum_1/03_testing/LGG.raxml.log"
+f3=03_testing_$training/$new_model"_G_full_aln.raxml.log"
+f4="../phylum_1/03_testing/LGG_full.raxml.log"
+declare -a filenames=($f1 $f2 $f3 $f4)
+
+# Create a new file to store the table
+log_file="03_testing_"$training"/log_table.txt"
+
+# Print the header of the table
+echo -e "Likelihood\tAIC\tTime\tFilename" > "$log_file"
+
+# Loop through each specified file
+for file in "${filenames[@]}"; do
+    # Check if the file exists
+    if [[ -f "$file" ]]; then
+        # Extract the required values using grep and awk
+        likelihood=$(grep 'Final LogLikelihood' "$file" | awk '{print $3}')
+        aic=$(grep 'AIC score' "$file" | awk '{print $3}')
+        time=$(grep 'Elapsed time' "$file" | awk '{print $3}')
+        filename=$(basename "$file")
+
+        # Append the extracted values to the log file
+        echo -e "$likelihood\t$aic\t$time\t$filename" >> "$log_file"
+    else
+        echo "The file $file does not exist." >> "$log_file"
+    fi
+done
+
+# Display the table
+cat "$log_file"
+
+cd ..
+
+echo "" >> $log
+echo "Comparing fit of new model on full alignment and r207 tree" >> $log
+
+cat 03_testing/$log_file >> $log
